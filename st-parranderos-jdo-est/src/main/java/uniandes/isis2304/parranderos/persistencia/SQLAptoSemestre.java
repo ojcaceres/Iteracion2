852dc1af0b88928f06c5412dba16eee2a018791e
nicoleduenas <nv.duenas@uniandes.edu.co>
2020-05-02 19:21:04 -0500

    prueba

--- st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/persistencia/SQLAptoSemestre.java
+++ st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/persistencia/SQLAptoSemestre.java
@@ -33,7 +33,7 @@ class SQLAptoSemestre
 	/* ****************************************************************
 	 * 			Constantes
 	 *****************************************************************/
-	/**
+	/**cjkdancjdsknc
 	 * Cadena que representa el tipo de consulta que se va a realizar en las sentencias de acceso a la base de datos
 	 * Se renombra acá para facilitar la escritura de las sentencias
 	 */
